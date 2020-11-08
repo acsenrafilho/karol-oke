dcde7bd0ef5acc6e2dedab5e5cd43407f586a21b
Antonio Filho <acsenrafilho@gmail.com>
2020-11-08 11:25:51 -0300

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Karol-Oke',
-      version='0.0.1',
+      version='0.0.2',
       description='Simple framework for Karoke',
       author='Antonio Filho',
       author_email='acsenrafilho@gmail.com',
